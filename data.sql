fa68b65bb1a4fbf7671df44db7fcee0ec9ffee88
Mahdi Noori <mahdinoori2389@gmail.com>
2023-08-21 21:12:20 +0430

    Add animals data to the data.sql

--- data.sql
+++ data.sql
@@ -3,3 +3,10 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+-- Insert Animals data into animals table
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+                           VALUES('Agumon', '2020-02-05', 0, TRUE, 10.23), 
+                           VALUES('Gabumon', '2018-11-15', 2, TRUE, 8), 
+                                 ('Pikachu', '2021-01-07', 1, FALSE, 15.04), 
+                                 ('Devimon', '2017-05-12', 5, TRUE, 11);
