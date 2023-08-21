106e1ca6375a2af37f9598e7b8cb7cd247f77568
Mahdi Noori <mahdinoori2389@gmail.com>
2023-08-22 00:35:47 +0430

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,5 +7,5 @@ SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg >10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
-SELECT * FROM animals WHERE name = 'Gabumon';
+SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
